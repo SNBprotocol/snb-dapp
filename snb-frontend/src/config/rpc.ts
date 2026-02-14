9f338c9535c5c86b3f8bb837ccef5ce44289f146
SNB Protocol <q15206065949@gmail.com>
2026-02-14 12:29:49 +0800

    Update rpc.ts
    Update rpc.ts

--- snb-frontend/src/config/rpc.ts
+++ snb-frontend/src/config/rpc.ts
@@ -10,8 +10,8 @@ export const RPC_URLS: Record<number, string[]> = {
   ],
 
   [CHAIN_ID.BSC_MAINNET]: [
-    "https://rpc.ankr.com/bsc",
     "https://bsc.publicnode.com",
     "https://bsc-dataseed.binance.org",
+    "https://bsc-dataseed1.binance.org",
   ],
 };
